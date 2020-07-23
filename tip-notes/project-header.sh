9d3470f680f98d5b96d0a6268835bfc851c8fe85
S. Falcao <falecomigo.falcao@gmail.com>
2020-07-23 20:13:30 -0300

    add project binaries and links destination folders

--- tip-notes/project-header.sh
+++ tip-notes/project-header.sh
@@ -11,7 +11,11 @@
 #===============================================================================
 
 
-__project_name__="tip notes"
-__project_version__="v0.1.0"
+__project_name__="tip-notes"
+__project_version__="0.1.0"
 __project_mail__="falecomigo.falcao@gmail.com"
 __project_dev_stage__="DRAFT"
+
+
+__project_binaries_folder__="/usr/local/bin/${__project_name__}"
+__project_links_folder__='/usr/local/bin'
