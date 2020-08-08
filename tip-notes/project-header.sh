08df8514957e38b8d4ec945e25d6ea0acdcb46db
S. Falcao <falecomigo.falcao@gmail.com>
2020-08-08 18:44:44 -0300

    change postisnt and postrm creation
    Now on postinst and postrm are generated rather then copied.
    The advantage of this approach is that most changes in
    project-header.sh would be automatically propagated to these
    files

--- tip-notes/project-header.sh
+++ tip-notes/project-header.sh
@@ -35,16 +35,14 @@ __project_mail__="falecomigo.falcao@gmail.com"
 __project_dev_stage__="DRAFT"
 
 
-__project_binaries_folder__="/usr/local/bin/${__project_name__}"
+__project_binaries_folder__="/usr/lib/${__project_name__}"
 __project_links_folder__='/usr/local/bin'
+__project_doc_folder__="/usr/share/doc/${__project_name__}"
+__project_autocompletion_file__="/etc/bash_completion.d/tip-autocomplete"
 
-
+# if change the following line, change also deb-make purge section
 tip_config_folder=~/.config/tip-notes
 tip_config_file=${tip_config_folder}/tiprc
 
 # defaults (may be changed after sourcing configuration file)
 tip_editor=''
-#tip_confirm_remove=true
-#tip_confirm_remove_purge=true
-#tip_confirm_config_reset=true
-#tip_confirm_delete_note=true
